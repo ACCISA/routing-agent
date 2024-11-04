290ed987b6030d7e8cf3fb5b8b5ca1e0640199fa
ACCISA <aniss.chalah@gmail.com>
2024-11-03 22:50:34 -0500

    config read bug

--- console/ssl_connect.py
+++ console/ssl_connect.py
@@ -28,16 +28,28 @@ data2 = bytes([
     0x44, 0x51, 0xed, 0xaf, 0xf9, 0x14, 0xb5, 0x2f, 0xc4, 0xa7, 0x9b, 0xeb, 0xc7, 0x5a, 0xbd, 0x5c
 ])
 
+forward_hostname = bytes([
+    0x00, 0x01, 0x00, 0x01,
+    0x01, 0x00, 0x00, 0x00, 
+    0x01, 0x00, 0x00, 0x00, 
+    0x00, 0x00, 0x00, 0x00, 
+    0x00, 0x00, 0x00, 0x00, 
+    0x08, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x00, 
+    0x3b, 
+    0x41, 0x31, 0x2c, 0x41, 0x32, 0x2c, 0x41, 0x33,
+    0x41, 0x32, 0x2c, 0x41, 0x33, 0x2c, 0x41, 0x33
+])
+
 
 print(bytes(data))
-print("data_len", len(data))
+print("data_len", len(forward_hostname))
 
 cipher = AES.new(key, AES.MODE_CBC, iv)
-encrypted_data = cipher.encrypt(pad(data, AES.block_size))
+encrypted_data = cipher.encrypt(pad(forward_hostname, AES.block_size))
 cipher2 = AES.new(key, AES.MODE_CBC, iv)
 encrypted_data2 = cipher2.encrypt(pad(data2, AES.block_size))
 
-hex_data = ''.join(f'{byte:02x}' for byte in data)
+hex_data = ''.join(f'{byte:02x}' for byte in forward_hostname)
 enc_hex_data = ''.join(f'{byte:02x}' for byte in encrypted_data)
 print(hex_data)
 print("header",hex_data[0:28])
@@ -45,9 +57,8 @@ print("payload", hex_data[28:])
 print(enc_hex_data)
 
 for i in range(0,10):
-    if i == 9:
-        print("running healthcheck")
-        encrypted_data = encrypted_data2
+    if i != 0:
+        break
 
     context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
     context.check_hostname = False
